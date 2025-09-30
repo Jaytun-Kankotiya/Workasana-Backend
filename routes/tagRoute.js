62f53b2a434347b4545891aeb88fac022da2789f
Jaytun Kankotiya <129341341+Jaytun-Kankotiya@users.noreply.github.com>
2025-09-30 13:21:55 -0400

    Update tagRoute.js

--- routes/tagRoute.js
+++ routes/tagRoute.js
@@ -1,4 +1,4 @@
-import { addNewTag, fetchAllTags } from "../controllers/tagController.js ";
+import { addNewTag, fetchAllTags } from "../controllers/tagController.js";
 import express from "express";
 
 const tagRouter = express.Router()
