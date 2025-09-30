b3f3b061ca260be67971a4a818453d876809e360
Jaytun Kankotiya <jaytunkankotiya81@gmail.com>
2025-09-30 15:15:28 -0400

    Cross-origin

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 
-const allowedOrigins = ['http://localhost:5173', 'https://workasana-backend-olive.vercel.app']
+const allowedOrigins = ['http://localhost:5173', 'https://workasana-frontend-prfj.vercel.app']
 
 const corsOptions = {
     origin: allowedOrigins,
